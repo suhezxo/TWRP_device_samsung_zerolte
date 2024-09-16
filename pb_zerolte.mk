a97528a2c12c7a2ff43a7b7d44417c3b67d6e13e
suhzxo <pentakotasuhas@gmail.com>
2024-09-16 10:24:03 +0530

    Update pb_zerolte.mk

--- pb_zerolte.mk
+++ pb_zerolte.mk
@@ -4,9 +4,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 
-# Inherit from device
-$(call inherit-product, device/<path>/device.mk) -- path to main device makefile
-
 # Inherit common product files.
 $(call inherit-product, vendor/pb/config/common.mk)
 
