94e1ba191699f8d8b9f787b5b49b552e1029d70e
suhzxo <pentakotasuhas@gmail.com>
2024-09-21 20:52:19 +0530

    Update omni_zerolte.mk

--- omni_zerolte.mk
+++ omni_zerolte.mk
@@ -1,8 +1,8 @@
 # Inherit from the common Open Source product configuration
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
-# Inherit from our custom product configuration
-$(call inherit-product, vendor/omni/config/common.mk)
+# Inherit common product files.
+$(call inherit-product, vendor/pb/config/common.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := zerolte
